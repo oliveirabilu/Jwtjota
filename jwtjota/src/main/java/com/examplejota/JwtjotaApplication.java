5bb2a6956e741e3180e82f6bbe7bd88791aa6406
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-03-31 16:27:43 -0300

    AprendendoCommit novo exercicio projeto Jwtjota

---
+++ jwtjota/src/main/java/com/examplejota/JwtjotaApplication.java
@@ -0,0 +1,13 @@
+package com.examplejota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtjotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtjotaApplication.class, args);
+	}
+
+}
---
+++ jwtjota/src/test/java/com/examplejota/JwtjotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplejota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtjotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
