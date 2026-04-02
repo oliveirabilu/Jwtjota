062e532f5afb1136e59b8765b6c0b8e07bb169b0
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-04-02 09:04:46 -0300

    feat.:Criado uma interface que implementa o userDetailsService que irá buscar o nosso usuario no banco, já que o spring não sabe em qual banco ele está e o userDetailService faz essa busca e retorna um UserDetails

---
+++ jwtjota/src/main/java/com/examplejota/service/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.examplejota.service;
+
+import com.examplejota.repositories.UserRepository;
+import com.examplejota.security.UserAuthenticated;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService{
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(UserAuthenticated::new)
+                .orElseThrow(()-> new UsernameNotFoundException("não valido"));
+    }
+}
